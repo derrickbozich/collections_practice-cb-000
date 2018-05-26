091c2dfb2415c0e5a9a220d222a7ed3da50d2a28
derrick b <dbozich1@gmail.com>
2018-05-26 01:33:47 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -37,10 +37,7 @@ end
 
 def add_s(array)
   array.each_with_index.collect do |element, index|
-    if index = 1
-      element.concat("")
-    else
-      element.concat("s")
+      element[index].concat("s")
     end
   end
 end
