348763c0db5c0b4d99460050b9897385ab44c431
derrick b <dbozich1@gmail.com>
2018-05-26 00:54:55 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,38 @@
+def sort_array_asc(array)
+  sorted_array = array.sort
+end
+
+def sort_array_desc(array)
+  sorted_array = array.sort {|a,b| b <=> a}
+end
+
+def sort_array_char_count(array)
+  array.sort do |a,b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  temp = array[1]
+  array[1] = array[2]
+  array[2] = temp
+  array
+end
+
+def reverse_array(array)
+  reversed_array = array.reverse
+end
+
+def kesha_maker(array)
+  array.each { |e| e[2] = "$" }
+end
+
+def find_a(array)
+  array.select { |e| e.start_with?("a")}
+end
+
+def sum_array(array)
+  array.inject(0) { |result, element| result + element }
+  
+end
 
