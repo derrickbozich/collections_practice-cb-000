c9e26e4178ed408d1562f0819734db548dd87381
derrick b <dbozich1@gmail.com>
2018-05-26 01:36:51 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -36,5 +36,5 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect {|element, index| e.concat("s")}
+  array.each_with_index.collect {|element, index| element.concat("s")}
 end
